49ab12ca5453c25bd6e7f9f31fb3f3837a24f9b6
Josue Kouka <josuebrunel@gmail.com>
2015-07-27 09:14:00 +0200

    flexible run.py

--- server/run.py
+++ server/run.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
-
+import pdb
+import sys
 from app import app
 
 if '__main__' == __name__:
-    app.run('0.0.0.0', port=8585)
+
+    try:
+        host = str(sys.argv[1])
+    except:
+        host = '0.0.0.0'
+    
+    try:
+        port = int(sys.argv[2])
+    except:
+        port = 8585
+
+    app.run(host, port=port)
